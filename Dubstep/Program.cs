292f35acbf0625bbadc209dd2a508c40801dd07b
Tim May <timmay1123@gmail.com>
2020-08-15 09:07:16 +1200

    Add project files.

---
+++ Dubstep/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dubstep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Dubstep:");
+
+            var dubstep = Console.ReadLine();
+            Console.WriteLine(SongDecoder(dubstep));
+            Console.ReadLine();
+        }
+
+        public static string SongDecoder(string input)
+        {
+            input = input.Replace("WUB", " ");
+            while (input.Contains("  "))
+            {
+                input = input.Replace("  ", " ");
+            }
+            return input.Trim();
+        }
+    }
+}
